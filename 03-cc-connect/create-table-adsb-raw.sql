3b420e3515faa403f6868d16d9fedebdf981a7e7
Hellmar Becker <becker@hellmar-becker.de>
2024-05-20 18:41:16 +0200

    Reorder options for easier copypaste

--- 03-cc-connect/create-table-adsb-raw.sql
+++ 03-cc-connect/create-table-adsb-raw.sql
@@ -8,13 +8,13 @@ CREATE TABLE `adsb-raw` (
   'topic' = 'adsb-raw',
   'properties.group.id' = 'kafka-supervisor-flink',
   'scan.startup.mode' = 'earliest-offset',
-  'properties.bootstrap.servers' = '<Confluent Cloud bootstrap server>',
-  'properties.security.protocol' = 'SASL_SSL',
-  'properties.sasl.mechanism' = 'PLAIN',
-  'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule  required username="<Confluent Cloud API Key>" password="<Confluent Cloud API Secret>";',
   'key.format' = 'csv',
   'key.fields' = 'kafka_key', 
   'key.fields-prefix' = 'kafka_', 
   'value.format' = 'raw', 
-  'value.fields-include' = 'EXCEPT_KEY'
+  'value.fields-include' = 'EXCEPT_KEY',
+  'properties.bootstrap.servers' = '<Confluent Cloud bootstrap server>',
+  'properties.security.protocol' = 'SASL_SSL',
+  'properties.sasl.mechanism' = 'PLAIN',
+  'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule  required username="<Confluent Cloud API Key>" password="<Confluent Cloud API Secret>";'
 );
